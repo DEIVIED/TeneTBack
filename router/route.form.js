cb402ed25edfc55fbd4636033078a751fe7bb1fa
Seynabou Niang <seynabouniangseyna@gmail.com>
2022-03-31 14:52:30 +0000

    CRUD for Users

--- router/route.form.js
+++ router/route.form.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../model/users.model');
 const userService = require('../services/user.service')(User);
+const auth = require('../helpers/auth');
+const { openDelimiter } = require('ejs');
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.json({ user: 'Ok' });
-  console.log('Test ok');
 });
 
 router.post('/signUp', async (req, res, next) => {
@@ -19,16 +21,6 @@ router.post('/signUp', async (req, res, next) => {
   }
 });
 
-// router.post('/login', async (req, res, next) => {
-//   try {
-//     let { email, password, profil } = req.body;
-//     res.json(await essai.login(email, password, profil));
-//   } catch (error) {
-//     next(error)
-//   }
-
-// });
-
 router.post('/signIn', async (req, res, next) => {
   try {
     let { email, password, profil } = req.body;
@@ -39,19 +31,80 @@ router.post('/signIn', async (req, res, next) => {
 
 });
 
-// router.get("/login", formController.logPage);
+router.put('/updateUser/:id', auth, async function (req, res, next) {
+  const authUser = await req.auth.userId;
 
-// router.post('/logOut_form', formController.logOut);
+  const newUser = {
+    // _id:req.params.id,
+    ...req.body
+  };
+  try {
+    const result = await userService.updateUser(req.params.id, newUser, authUser)
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
 
-// router.post('/signUp', helpers.verifEmailAndNumero, formController.signUp);
+});
 
-// router.post('/password_form', formController.changePassword)
-// router.get('/profil', auth, formController.profilPage);
-// router.get('/userHome', auth, formController.userPage);
-// router.get('/password', auth, formController.passwordPage);
-// router.post('/profilUser_form', auth, formController.saveProfil);
+// router.put('/changeYourPassword/:id',auth ,async function (req,res,next){
+//   console.log('ok');
+//   const authUser = await req.auth.userId;
+//   try {
+//     let { old,newPass } = req.body;
+//     const result = await userService.changePassword(req.params.id,old,newPass,authUser)
+//     res.json(result)
+//   } catch (error) {
+//     next(error)
+//   }
 
+// })
 
+router.get('/diplayUser/:id', async function (req, res, next) {
+  const userId = req.params.id;
+  try {
+    let user = await userService.getUserById(userId)
+    res.json(user);
+  } catch (error) {
+    next(error)
+  }
+});
+
+// router.get('/displayAnotherUser/:id',async function (req, res, next) {
+//   const userId = req.params.id;
+//   try {
+//     let user = await userService.getUserById(userId)
+//     res.json(user);
+//   } catch (error) {
+//     next(error)
+//   }
+// });
+
+router.get('/users', async function (req, res, next) {
+  try {
+    const response = await userService.getAllUsers()
+    res.json(response)
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.delete('/deleteYourAccount/:id', auth, async function (req, res, next) {
+  try {
+    const deleteCount = await userService.deleteUser(req.params.id, req.auth.userId)
+    res.json(deleteCount)
+  } catch (error) {
+    next(error)
+  }
+});
+router.delete('/deleteUser/:id', auth, async function (req, res, next) {
+  try {
+    const deleteCount = await userService.deleteUserByAdmin(req.params.id, req.auth.userId)
+    res.json(deleteCount)
+  } catch (error) {
+    next(error)
+  }
+});
 
 
 module.exports = router;
--- services/user.service.js
+++ services/user.service.js
@@ -60,30 +60,198 @@ const authenticate = User => async (email, password, profil) => {
     }
 }
 
-const updateProfil = User => async (id) => {
-    const profil = await User.findByIdAndUpdate(id, { profiluser: req.body })
-        .then(() => {
-            res.status(201).json({
-                message: 'profil saved successfully!'
-            });
-        }).catch(
-            error => {
-                res.status(400).json({
-                    error: error
+const getUserById = User => async (id) => {
+    try {
+        const user = await User.findOne({ _id: id });
+        if (user) {
+            return ({
+                status: 'success',
+                message: 'User found',
+                payload: user
+            })
+        }
+        else {
+            return ({
+                status: 'fail',
+                message: 'User not found',
+                payload: null
+            })
+        }
+
+    } catch (error) {
+        return ({
+            status: "error",
+            message: error,
+            payload: null
+        });
+    }
+
+}
+
+const updateUser = User => async (id, newUser, reqAuthUserId) => {
+    // const _user = new User({_id:id,...newUser});
+    try {
+        const user = await User.findOne({ _id: id })
+        if (!user) {
+            return ({
+                status: "error",
+                message: "user not found",
+                payload: null
+            })
+        }
+        else if (id !== reqAuthUserId) {
+            // console.log('3');
+            return ({
+                status: "error",
+                message: "Unauthorized request!",
+                payload: null
+            })
+        }
+        else if (id === reqAuthUserId) {
+            // console.log('4');
+            const update = await User.updateOne({ _id: id }, { ...newUser })
+
+            if (!update) {
+                return ({
+                    status: "error",
+                    message: "failed changes!",
+                    payload: null
+                })
+
+            }
+            else {
+                const userUpdate = await User.findOne({ _id: id })
+                // console.log('5',userUpdate);
+                return ({
+                    status: "success",
+                    message: "post edited succesfully!",
+                    payload: userUpdate
                 });
             }
-        )
+        }
+    } catch (error) {
+        return ({
+            status: "error",
+            message: error,
+            payload: null
+        });
+    }
+
 }
 
-const getUserById = User => async (id) => {
+const deleteUser = User => async (id, reqAuthUserId) => {
+    try {
+        const user = await User.findOne({ _id: id })
+        if (!user) {
+            return ({
+                status: "error",
+                message: "user not found!",
+                payload: null
+            })
+        }
+        else if (id !== reqAuthUserId) {
+            return ({
+                status: "error",
+                message: "Unauthorized request!",
+                payload: null
+            })
+        }
+        else if (id === reqAuthUserId) {
+            const deleteUser = await User.deleteOne({ _id: id })
+            if (!deleteUser) {
+                return ({
+                    status: "error",
+                    message: "failed changes!",
+                    payload: null
+                })
+
+            } else {
+                return ({
+                    status: "success",
+                    message: "user deleted succesfully!",
+                    payload: null
+                });
+
+            }
+        }
+
+    } catch (error) {
+        return ({
+            status: 'fail',
+            message: 'Sorry',
+            payload: error
+        });
+    }
+}
+
+
+const getAllUsers = User => async () => {
+    try {
+        const users = await User.find();
+        if (users) {
+            return ({
+                status: 'success',
+                message: 'All Users',
+                payload: users
+            });
+        }
+    } catch (error) {
+        return ({
+            status: 'fail',
+            message: 'Sorry',
+            payload: error
+        });
+    }
 
 }
 
-const updateUser = User => (id, newUser) => {
+const deleteUserByAdmin = User => async (id, adminId) => {
+    const admin = await User.findOne({ _id: adminId })
+    if (admin.profil === 'admin') {
+        try {
+            const user = await User.findOne({ _id: id })
+            if (!user) {
+                return ({
+                    status: "error",
+                    message: "user not found!",
+                    payload: null
+                })
+            }
 
-}
+            else {
+                const deleteUser = await User.deleteOne({ _id: id })
+                if (!deleteUser) {
+                    return ({
+                        status: "error",
+                        message: "failed!",
+                        payload: null
+                    })
 
-const deleteUser = User => async (id) => {
+                } else {
+                    return ({
+                        status: "success",
+                        message: "user deleted succesfully!",
+                        payload: null
+                    });
+
+                }
+            }
+
+        } catch (error) {
+            return ({
+                status: 'fail',
+                message: 'Sorry',
+                payload: error
+            });
+        }
+    }
+    else {
+        return ({
+            status: "error",
+            message: "Unauthorized request!",
+            payload: null
+        })
+    }
 
 }
 
@@ -97,7 +265,9 @@ module.exports = (User) => {
         authenticate: authenticate(User),
         getUserById: getUserById(User),
         updateUser: updateUser(User),
-        deleteUser: deleteUser(User)
+        deleteUser: deleteUser(User),
+        getAllUsers: getAllUsers(User),
+        deleteUserByAdmin: deleteUserByAdmin(User)
 
     });
 };
